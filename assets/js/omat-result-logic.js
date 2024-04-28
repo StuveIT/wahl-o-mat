6c84953357322f0ec40e9b51bd5d59ba61e81fe9
Anton Pogrebnjak <anton.pogrebnjak@uni-konstanz.de>
2024-04-28 14:43:20 +0200

    Revamped backend to function according to [Rechenmodell_des_Wahl-O-Mat.pdf](https://www.bpb.de/system/files/dokument_pdf/Rechenmodell_des_Wahl-O-Mat.pdf)

--- assets/js/omat-result-logic.js
+++ assets/js/omat-result-logic.js
@@ -1,29 +1,3 @@
-class Vector {
-
-    constructor(answers, weights) {
-        this.answers = answers;
-        this.weights = weights;
-
-        this.vector = [];
-        for (let i = 0; i < answers.length; i++) {
-            const answer = answers[i];
-            
-            if(answer != -1 && answer != 0 && answer != 1) {
-                this.vector[i] = null;
-                continue;
-            }
-
-            if(weights !== null && weights[i] == 1) {
-                this.vector[i] = answer * 2;
-                continue;
-            }
-
-            this.vector[i] = answer;
-        }
-    }
-}
-
-// fetch parties from database
 async function fetchParties() {
     const response = await fetch('/api/parties');
     const data = await response.json();
@@ -31,61 +5,108 @@ async function fetchParties() {
     return data;
 }
 
-// compute distance between two vectors
-function computeDistance(vector1, vector2) {
-    let sum = 0;
-    for(let i = 0; i < vector1.length; i++) {
-        if(vector1[i] == null || vector2[i] == null)
-            continue;
+async function fetchTheses() {
+    const response = await fetch('/api/theses');
+    const data = await response.json();
 
-        sum += Math.abs(vector1[i] - vector2[i]);
-    }
-
-    return Math.sqrt(sum);
+    return data;
 }
 
+const valueGrid = [
+    [2, 1, 0],
+    [1, 2, 1],
+    [0, 1, 2]
+]
 
 async function main() {
-    const userVector = new Vector(appAnswers, appWeights);
     let parties = await fetchParties();
+    let theses = await fetchTheses();
 
-    // generate party vectors and compute distances
-    parties.forEach(party => {
-        party.vector = new Vector(party.answers, appWeights).vector;
-        party.distance = computeDistance(userVector.vector, party.vector);
-    });
+    let pointGrid = [];
 
-    let virtualAnswerCount = appAnswers.length;
-    appWeights.forEach((weight, index) => {
-        if(weight == 1)
-            virtualAnswerCount++;
-    });
+    // appAnswers is given by the ejs: holds the answers of the user
+    // appWeights is given by the ejs: holds the weights of the answers
 
-    // sort parties by distance
-    const sortedParties = parties.sort((a, b) => {
-        return a.distance - b.distance;
-    });
+    // make a point grid of size: thesis_count x (party_count + 1 (for the max value))
+    for (let row = 0; row < theses.length; row++) {
+        pointGrid[row] = [];
+        userAnswer = appAnswers[row];
+
+        // if the user skipped the question, the row should be 0
+        if (userAnswer == null) {
+            for(let col = 0; col < parties.length; col++) {
+                pointGrid[row][col] = 0;
+            }
+
+            // add the max value for the user
+            pointGrid[row][parties.length] = 0;
+
+            continue; // skip the rest of the loop
+        }
+
+        // otherwise calculate the points for the user and party combination
+        for (let col = 0; col < parties.length; col++) {
+            party = parties[col];
+            partyAnswer = theses[row][party.pid];
+
+            // calculate the points for the user and party combination
+            pointGrid[row][col] = valueGrid[userAnswer][partyAnswer] * appWeights[row];
+        }
+
+        // add the max value for the user
+        pointGrid[row][parties.length] = 2 + 2 * appWeights[row];
+    }
+
+    let lastRow = pointGrid.length - 1;
+
+    // calculate column sums
+    pointGrid[lastRow + 1] = [];
+    for(let col = 0; col < parties.length + 1; col++) { // + 1 for the max value
+        let sum = 0;
+        for(let row = 0; row < theses.length; row++) {
+            sum += pointGrid[row][col];
+        }
+
+        pointGrid[lastRow + 1][col] = sum;
+    }
+
+    // calculate the percentage of the user and party combination
+    let results = [];
+    for(let col = 0; col < parties.length; col++) {
+        let sum = pointGrid[lastRow + 1][col];
+        let max = pointGrid[lastRow + 1][parties.length];
+
+        results[col] = {
+            party: parties[col],
+            percentage: sum / max
+        }
+    }
+
+    // sort the parties by the percentage
+    results.sort((a, b) => b.percentage - a.percentage);
+    console.log(results);
 
     // render results
     const resultContainer = document.getElementById('output');
-    sortedParties.forEach(party => {
+    results.forEach(result => {
+        const party = result.party;
+        const percentage = result.percentage;
+
         const partyContainer = document.createElement('div');
         partyContainer.classList.add('party');
 
         const partyName = document.createElement('h2');
-        partyName.innerText = party.name;
+        partyName.innerText = party.party;
         partyContainer.appendChild(partyName);
 
         const partyDescription = document.createElement('p');
         partyDescription.innerText = party.description;
         partyContainer.appendChild(partyDescription);
 
-        let percentage = Math.round((1 - party.distance / virtualAnswerCount) * 100);
-
         const partyPercentage = document.createElement('span');
         partyPercentage.classList.add('percentage');
-        partyPercentage.style.width = percentage + '%';
-        partyPercentage.innerText = 'Übereinstimmung: ' + percentage + '%';
+        partyPercentage.style.width = (percentage * 100) + '%';
+        partyPercentage.innerText = 'Übereinstimmung: ' + (percentage * 100) + '%';
         partyContainer.appendChild(partyPercentage);
 
         resultContainer.appendChild(partyContainer);
